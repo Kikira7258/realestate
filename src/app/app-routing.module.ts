eb7bf66a2ba1e38bb954452bc23d903e433aaa4e
Anita Baker <anitabaker@myambergroup.com>
2025-07-20 10:08:58 -0500

    first commit - environment setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/client-view/home/home.component';
+import { AboutComponent } from './views/client-view/about/about.component';
+import { FaqComponent } from './views/client-view/faq/faq.component';
+import { ContactComponent } from './views/client-view/contact/contact.component';
+import { ListingsComponent } from './views/client-view/listings/listings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent},
+  { path: 'faqs', component: FaqComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'listings', component: ListingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './views/client-view/home/home.component';
+import { AboutComponent } from './views/client-view/about/about.component';
+import { FaqComponent } from './views/client-view/faq/faq.component';
+import { ContactComponent } from './views/client-view/contact/contact.component';
+import { ListingsComponent } from './views/client-view/listings/listings.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    FaqComponent,
+    ContactComponent,
+    ListingsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
